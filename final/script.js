4a1725dc52bbf1869c07489f369a4fafc75a1752
Anna Zhao <azzzhao@ucdavis.edu>
2023-12-06 00:35:58 -0800

    final project updated

--- final/script.js
+++ final/script.js
@@ -3,15 +3,14 @@
     console.log('reading js'); 
 
     //assigning variables and getting all the elements needed for script
-    // variables for image and divs 
-    const theImg = document.querySelector('img'); 
-    const hotspot = document.querySelectorAll('#legoimage div');
+    // variables for hotspots
+    const friendshotspot = document.querySelectorAll('#friendslego div');
 
-    //variable for all overlays
-    const overlays = document.querySelectorAll('section article'); 
+    //variable for overlays
+    const friendsOverlays = document.querySelectorAll('#friendsover article'); 
 
     // varible for the 'close' button 
-    const close = document.querySelectorAll('button'); 
+    const closebut = document.querySelectorAll('.closebutton'); 
 
     //variables for each overlay
     const ros = document.querySelector('#rossover');
@@ -19,41 +18,22 @@
     const rach = document.querySelector('#rachelover');
     const joe = document.querySelector('#joeyover');
     const chan = document.querySelector('#chandlerover');
-    const phoe = document.querySelector('#phoeberover');
-    const gunth = document.querySelector('#guntherrover');
+    const phoe = document.querySelector('#phoebeover');
+    const gunth = document.querySelector('#guntherover');
 
-    //after 5s, title/instruction fades out
-    setTimeout(function(){document.querySelector('h2').style.color = 'rgb(17, 8, 3)';}, 5000); 
-
-    //when a character is clicked, run the funtions to zoom in and show the overlays 
-    hotspot.forEach(function(eachSpot){
-        eachSpot.addEventListener('click', zoomIn); 
+    //when a character is clicked, run the funtion and show the overlays 
+    friendshotspot.forEach(function(eachSpot){
         eachSpot.addEventListener('click', showoverlays); 
     }); 
 
     //for each overlay, close the overlay when 'close' button is clicked
-    close.forEach(function(eachBut){
+    closebut.forEach(function(eachBut){
         eachBut.addEventListener('click', function(){
-            for (let i = 0; i<overlays.length; i++) {
-                overlays[i].className = 'hide'; 
-                theImg.className = 'start'
+            for (let i = 0; i<friendsOverlays.length; i++) {
+                friendsOverlays[i].className = 'hide'; 
             }
         }); 
     }); 
-
-    //function to zoom in and show the details of each character
-    function zoomIn(event) {
-        const spot = event.target.id;
-        switch (spot) {
-            case 'Ross': theImg.className = 'ross'; break;
-            case 'Monica': theImg.className = 'monica'; break;
-            case 'Rachel': theImg.className = 'rachel'; break;
-            case 'Joey': theImg.className = 'joey'; break;
-            case 'Chandler': theImg.className = 'chandler'; break;
-            case 'Phoebe': theImg.className = 'phoebe'; break;
-            case 'Gunther': theImg.className = 'gunther';
-        }
-    }; 
 
     //function to show the overlays for each charatcer on click
     function showoverlays (event) {
